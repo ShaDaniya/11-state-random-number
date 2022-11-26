dfcbff2bbf4b482b77516b805083e715235f09c3
Daniya <daniyashagieva@gmail.com>
2022-11-26 22:01:32 +0300

    made separate function generateRandom Num

---
+++ src/components/RandomNumber.js
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import generateRandomNum from '../utils/generateRandomNum'
+
+function RandomNumber() {
+    const [randomNum, setRandomNum] = useState(generateRandomNum())
+    const changeRandomNum = () => {
+        setRandomNum(generateRandomNum())
+    }
+
+    return (
+        <div>
+            <h1>{randomNum}</h1>
+            <button onClick={changeRandomNum}>Generate new random number</button>
+        </div>
+    )
+}
+
+export default RandomNumber
